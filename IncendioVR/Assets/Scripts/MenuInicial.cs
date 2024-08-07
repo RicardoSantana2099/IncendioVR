999634b2347cd0eee37740c6b3a86f2eb0ccf27d
PACHECO 2099 <122486806+RicardoSantana2099@users.noreply.github.com>
2024-08-07 11:40:59 -0600

    BotonInterfaz
    Se crearon los botones para la interfaz de inicio, estos deben permitir, iniciar la simulación, mostrar los créditos y salir de la aplicación.

---
+++ IncendioVR/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
